d5646716e37de12ea7f7307485c1ecf949a36049
Moslm Shlaka <mike7olwo@gmail.com>
2026-03-24 15:45:27 +0300

    gradle versions updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
 }
 
 dependencies {
-    val  compose_version = "1.0.2"
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,13 +49,14 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-
-//    implementation(libs.androidx.material3)
-    implementation ("com.google.android.material:material:1.4.0")
-    implementation ("androidx.compose.ui:ui:$compose_version")
-    implementation( "androidx.compose.material:material:$compose_version")
-    implementation( "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation( "androidx.activity:activity-compose:1.3.1")
+    implementation(libs.androidx.material3)
+//    implementation ("com.google.android.material:material:1.4.0")
+//    implementation ("androidx.compose.ui:ui:$compose_version")
+//    implementation( "androidx.compose.material:material:$compose_version")
+    // # icons extended
+//    implementation(libs.androidx.compose.material.icons.extended)
+//    implementation(libs.androidx.compose.material.icons.core)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -66,17 +66,14 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1") // Kotlin extensions and Coroutines support
-
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-    kapt("com.google.dagger:hilt-compiler:2.49")
+    // # Room DB
+    val room_version = "2.6.1" // Use latest stable version
+    val hilt_version = "2.51.1" // Use latest stable version
+    implementation("androidx.room:room-runtime:${room_version}")
+    implementation("androidx.room:room-ktx:${room_version}")  // Kotlin extensions and Coroutines support
+    // # Hilt
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    "kapt"("com.google.dagger:hilt-android-compiler:$hilt_version")
+    // Hilt for Jetpack (ViewModel integration)
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0") // For Hilt with Compose Navigation
-
-    // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 }
